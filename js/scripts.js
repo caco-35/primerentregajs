5fabf65a88ca57e0be2af382ad233434cb3cf1f0
Carlos Couste <cacotrico@gmail.com>
2024-09-20 10:50:49 -0300

    Correcciones
    Mejoritas

--- js/scripts.js
+++ js/scripts.js
@@ -22,10 +22,17 @@ const listarProductos = () => {
     return lista;
 }
 
-do {
+const finalizarCompra = () => {
+    let salir = confirm('Quiere finalizar su compra?')
+            if (salir){
+                let productosComprados = carrito.map(item => `${item.nombre} - $${item.precio}`).join('\n');
+                alert(`Gracias por tu compra!\n\nSu compra:\n${productosComprados}\n\nTotal de la compra: $${total}`);
+            }else{
+                opcion = null;
+            }
+}
 
-    opcion = prompt(listarProductos());
-
+const selectProductos = () => {
     switch (opcion) {
         case '1':
         case '2':
@@ -41,18 +48,19 @@ do {
             alert(`Has añadido ${productos[indice].nombre}.\n\nTotal: $${total}`);
             break;
         case '9':
-            let salir = confirm('Quiere finalizar su compra?')
-            if (salir){
-                let productosComprados = carrito.map(item => `${item.nombre} - $${item.precio}`).join('\n');
-                alert(`Gracias por tu compra!\n\nSu compra:\n${productosComprados}\n\nTotal de la compra: $${total}`);
-            }else{
-                opcion = null;
-            }
+            finalizarCompra();
             break;
         default:
             alert(error);
             break;
     }
+}
+
+
+do {
+
+    opcion = prompt(listarProductos());
+    selectProductos(opcion);
 
 }while (opcion !== '9');
 
